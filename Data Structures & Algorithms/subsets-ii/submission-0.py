e5a1fa5b26d3c7de53e97d08f7e3a63123513226
Train Chen <train981316@gmail.com>
2026-04-02 11:16:31 +0200

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        subset = []
+        nums.sort()
+        print(nums)
+
+        def backtracking(start: int, subset: List[int]):
+            result.append(subset.copy())
+
+            for i in range(start, len(nums)):
+
+                # check if this choice respects the boundary, only advance if it does
+                if nums[i] == nums[i - 1] and i > start:
+                    continue
+
+                # Everything down here is the "good" logic that runs for 
+                # the first instance of every number.
+                subset.append(nums[i])
+                    
+                # next choice
+                backtracking(i + 1, subset.copy())
+                subset.pop()
+
+        backtracking(0, subset)
+        return result
